b674412dcd2858e8110e9f2001105fa1499ab113
Angélique SAUVAGE <angelique.sauvage@ens.uvsq.fr>
2023-02-21 10:57:58 +0100

    ajout couleur interface graphique

---
+++ Projet-taquin.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+racine = tk.Tk() # Création de la fenêtre racine
+racine.title("affichage") # ajoute un titre
+label = tk.Label(racine, text="affichage", font=("helvetica", "20")) # création du widget
+label.grid() # positionnement du widget
+label.config(text="Jeu du Taquin", bg="saddle brown") # modification des paramètres du widget
+racine.mainloop() # Lancement de la boucle principale

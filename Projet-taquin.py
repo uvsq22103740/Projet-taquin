fbd3063d90d68206cca84c5eefc73de5138f257b
Angélique SAUVAGE <angelique.sauvage@ens.uvsq.fr>
2023-03-19 16:27:40 +0100

    création classe

--- Projet-taquin.py
+++ Projet-taquin.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-racine = tk.Tk() # Création de la fenêtre racine
-racine.title("affichage") # ajoute un titre
-label = tk.Label(racine, text="affichage", font=("helvetica", "20")) # création du widget
-label.grid() # positionnement du widget.
-label.config(text="Jeu du Taquin", bg="saddle brown") # modification des paramètres du widget
-racine.mainloop() # Lancement de la boucle principale
+
+class Taquin:
+    """On regroupe les données et des fonctionnalités dans une classe qu'on nomme Taquin"""
+    
+racine = tk.Tk()
+jeu = Taquin(racine)
+racine.mainloop()

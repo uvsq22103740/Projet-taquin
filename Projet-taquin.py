5c7f745ad68a7a4c8eefbef2c78e4dcb71769ce1
Angélique SAUVAGE <angelique.sauvage@ens.uvsq.fr>
2023-03-20 08:42:33 +0100

    comment

--- Projet-taquin.py
+++ Projet-taquin.py
@@ -1,16 +1,19 @@
 import tkinter as tk
-
+"""importation de la bibliothèque tkinter"""
 class Taquin:
     """On regroupe les données et des fonctionnalités dans une classe qu'on nomme Taquin"""
     def __init__(T, jeu):
+        """On utilise la méthode init pour effectuer des opérations lors de l'instanciation de notre classe"""
         T.jeu = jeu
         jeu.title("Jeu du Taquin")
-        """On utilise la méthode init pour effectuer des opérations lors de l'instanciation de notre classe"""
+        """On ajoute le titre Jeu du Taquin"""
         T.grid = tk.Frame(jeu, borderwidth=5, relief="solid")
         """ Création de la grille"""
         T.grid.pack()
         """permet de placer le cadre T.grid dans la fenêtre principale de jeu."""
 
 racine = tk.Tk()
+"""Création de la fenêtre racine"""
 jeu = Taquin(racine)
 racine.mainloop()
+"""Lancement de la boucle principale"""

555ed4f07efc88059606e67e455bdd5f5a3d9e09
uvsq22205300 <114034482+uvsq22205300@users.noreply.github.com>
2023-03-20 16:51:13 +0100

    Update Projet-taquin.py

--- Projet-taquin.py
+++ Projet-taquin.py
@@ -17,6 +17,16 @@ class Taquin:
             """Création d'une ligne de 4"""
             ligne = []
             """ on crée la variable "ligne" vide qui sera ensuite remplie"""
+            for j in range(4):
+                """Création des boutons avec lesquels le joueur pourra interagir par l'avenir"""
+                bouton = tk.Button(T.grid, width=8, height=4, font=('ROG Fonts', 20))
+                """Apparition du bouton sur l'interface graphique"""
+                bouton.grid(row=i, column=j)
+                """Affiliation des boutons pour les lignes puis les colonnes"""
+                ligne.append(bouton)
+                """Ajout des boutons dans le ligne"""
+            T.bouton.append(ligne)
+            """Permet d'affilier ces boutons sur toutes les lignes du 4x4"""
 racine = tk.Tk()
 """Création de la fenêtre racine"""
 racine.configure(bg="saddle brown")

c103c20154ae9fa32ed9365c785c09084f8eb87a
Angélique SAUVAGE <angelique.sauvage@ens.uvsq.fr>
2023-03-21 12:26:00 +0100

    bibli

--- Projet-taquin.py
+++ Projet-taquin.py
@@ -7,16 +7,31 @@ class Taquin:
         T.jeu = jeu
         jeu.title("Jeu du Taquin")
         """On ajoute le titre Jeu du Taquin"""
-        T.grid = tk.Frame(jeu, borderwidth=5, relief="solid")
+        T.grid = tk.Frame(jeu, borderwidth=10, relief=tk.RAISED)
         """ Création de la grille"""
         T.grid.pack()
         """permet de placer le cadre T.grid dans la fenêtre principale de jeu."""
-      
+        T.bouton = []
+        """Création d'une variable pour des cases vides"""   
+        for i in range(4):
+            """Création d'une ligne de 4"""
+            ligne = []
+            """ on crée la variable "ligne" vide qui sera ensuite remplie"""
+            for j in range(4):
+                """Création des boutons avec lesquels le joueur pourra interagir par l'avenir"""
+                bouton = tk.Button(T.grid, width=8, height=4, font=('ROG Fonts', 20))
+                """Apparition du bouton sur l'interface graphique"""
+                bouton.grid(row=i, column=j)
+                """Affiliation des boutons pour les lignes puis les colonnes"""
+                ligne.append(bouton)
+                """Ajout des boutons dans le ligne"""
+            T.bouton.append(ligne)
+            """Permet d'affilier ces boutons sur toutes les lignes du 4x4"""
 racine = tk.Tk()
-"""Création de la fenêtre racine"""
+"""Création de la fenêtre racine avec bibliothèque tkinter"""
 racine.configure(bg="saddle brown")
 """On donne la couleur marron a notre arrière plan"""
 jeu = Taquin(racine)
 """Création de la variable jeu"""
 racine.mainloop()
-"""Lancement de la boucle principale"""
+"""Lancement de la boucle principale du jeu"""

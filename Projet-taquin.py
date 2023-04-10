2d907f4ca857ead9f12a29d84b790e6cb95fbbca
uvsq22103740 <angelique.sauvage@ens.uvsq.fr>
2023-04-10 14:58:04 +0200

    amélioration des lignes des coms amélioration du bouton aide "?" plus efficasse pour interargir avec le joueur

--- Projet-taquin.py
+++ Projet-taquin.py
@@ -27,16 +27,16 @@ class Taquin:
             ligne = []
             """ on crée la variable "ligne" vide qui sera ensuite remplie"""
             for j in range(4):
-                """Création des boutons avec lesquels le joueur pourra interagir par l'avenir"""
-                bouton = tk.Button(T.grid, width=10, height=8, font=('ROG Fonts', 10),command=lambda ligne=i, colonne=j: T.bouge_case(ligne, colonne))
-                """Apparition du bouton sur l'interface graphique + on appelle la fonction bouge_case"""
+                """ Création bouton avec la commande "bouge_case" qui sera appelée avec les paramètres "ligne" et "colonne" pour chaque bouton"""
+                bouton = tk.Button(T.grid, bg="lemon chiffon", width=9, height=6, font=('ROG Fonts', 10),command=lambda ligne=i, colonne=j: T.bouge_case(ligne, colonne))
+                """Le bouton est placé dans la fenêtre principale à la position [ligne][colonne] (selon les boucles "for" ci-dessus)"""
                 bouton.grid(row=i, column=j)
                 """Affiliation des boutons pour les lignes puis les colonnes"""
                 ligne.append(bouton)
                 """Ajout des boutons dans le ligne"""
             T.bouton.append(ligne)
             """Permet d'affilier ces boutons sur toutes les lignes du 4x4"""
-        T.Aide = tk.Button(jeu, text="Aide",fg="RoyalBlue1",font=('ROG Fonts', 8), command=lambda:T.Aide_callback())
+        T.Aide = tk.Button(jeu, text="?",bg="RoyalBlue1",fg="Red2",font=('ROG Fonts', 38), command=lambda:T.Aide_callback())
         """Création du bouton AIDE """
         T.Aide.pack(side="bottom")
         T.Sauvegarde = tk.Button(jeu, text="Sauvegarder",bg="green3",font=('ROG Fonts', 10), command=lambda:T.Sauvegarde_callback())
@@ -52,7 +52,6 @@ class Taquin:
         bouton_annulation.pack(side="bottom")
         """Création bouton pour annuler le dernier mouvement"""
 
-    
     def Aide_callback(T):
             """Fonction appelée lorsque le bouton "Aide" est cliqué"""
             message = "Bienvenue dans le Jeu du Taquin.\n\n" \
@@ -68,7 +67,7 @@ class Taquin:
     def tableau_taquin(T):
         """création du tableau du taquin : va prendre aléatoirement un nombre entre 1 et 16"""
         nombre=list(range(1, 16,1))
-        "prend le nombre pioché aleatoirement entre 1 et 16"
+        "prend le nombre pioché aleatoirement entre 1 et 16, pas=1"
         rd.shuffle(nombre)
         """on utilise la méthode shuffle pour mélanger la liste"""
         tableau=[]
@@ -76,22 +75,27 @@ class Taquin:
         nombre.append(None)
         """Ajoute "rien" a la fin de la liste, le dernier élément est vide"""
         for o in range(0, len(nombre), 4):
-            """On parcourt de 4 en 4 le nombre d'élément de nombre"""
+            """On parcourt de 4 en 4 le nombre d'élément de nombre : pour crée liste de 4 sous-listes, chacune contenant 4 éléments de la liste aléatoire"""
             ligne = nombre[o:o+4]
             """On attribut à une ligne un nombre 4 fois car une ligne est composée de 4 cases"""
             tableau.append(ligne)
+            "on ajoute les lignes à tableau"
         T.renouvel_tableau(tableau)
+        """Afficher le tableau initial dans les boutons de la fenêtre principale"""
         return tableau
-        """retourne le tableau de la fonction"""
+        """Retourne la liste représentant le tableau initial"""
 
     def renouvel_tableau(T,tableau):
+        """Parcourt de chaque bouton dans la fenêtre principale"""
         for ligne in range(4):
             for colonne in range(4):
                 nombre = tableau[ligne][colonne]
+                """Stocke le nombre ou la valeur "None" dans la variable nombre"""
                 if nombre is None:
-                    T.bouton[ligne][colonne].configure(text="")
-                    """cette case est la case vide, il n'y a pas de nombre dedans"""
+                    """Si "nombre" est "None", effacer le texte du bouton"""
+                    T.bouton[ligne][colonne].configure(text="")    
                 else:
+                    """Sinon, on écrit le nombre dans le bouton"""
                     T.bouton[ligne][colonne].configure(text=str(nombre), font=("Rog fonts",10))
                     """un nombre par case on ajoute ligne au tableau et on met une police fun de taille 20"""
 
@@ -102,17 +106,16 @@ class Taquin:
 
     def message_du_gagnant():
         """on va définir le message du gagnant"""
-        message_gagnant="Jeu du Taquin\n\n" \
+        message_gagnant="Victoire!!!\n\n" \
                         "Félicitation tu es gagnant!"
-        tk.messagebox.showinfo(message_gagnant)
+        tk.messagebox.showinfo("Fin du jeu du Taquin", message_gagnant)
         """On réutilise la bibliothèque tkinter avec massagebox pour le message"""
 
-
     def bouge_case(T, ligne, colonne):
-        """on veut bouger les cases"""
+        """Déplace une case donnée par ses coordonnées (ligne, colonne) vers une case vide à coté"""
         if T.tableau[ligne][colonne] is None:
+            """cette case est la case vide, il n'y a pas de nombre dedans, il n'y a rien à faire"""
             return
-        """verifie si la case peut etre bougée"""
         if ligne > 0 and T.tableau[ligne-1][colonne] is None:  
             """verifie si la case peut etre bougée vers le HAUT"""
             T.tableau[ligne-1][colonne], T.tableau[ligne][colonne] = T.tableau[ligne][colonne], T.tableau[ligne-1][colonne]
@@ -130,8 +133,8 @@ class Taquin:
             T.tableau[ligne][colonne+1], T.tableau[ligne][colonne] = T.tableau[ligne][colonne], T.tableau[ligne][colonne+1]
             T.renouvel_tableau(T.tableau)
         if T.vérifie_réussite():
+            """Vérifie si le joueur a gagné après avoir déplacé une case. Si oui, affiche un message de victoire"""
             T.message_du_gagnant()
-
     
 racine = tk.Tk()
 """Création de la fenêtre racine avec bibliothèque tkinter"""
